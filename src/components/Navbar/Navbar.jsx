fe4bfffa06333ec89139fd5c7c9fdd34c2efdfb8
renan <renan.rodriguesmoura2022@gmail.com>
2022-12-02 14:40:37 -0300

    adicionando função search

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 
+import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";  
 import "./Navbar.css";
 
 
 const Navbar = () => {
+    const [search, setSearch] = useState("");
+    const navigate = useNavigate();
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+       if(!search) return;
+
+       navigate(`/search?q=${search}`);
+       setSearch("");
+    }
+    
     return (
         <nav id="navbar">
             <h2>
